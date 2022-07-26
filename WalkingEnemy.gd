8290e7215d7158236a9d98fafc0e1e2ee2277664
ChyavanShenoy <chyavandshenoy2318@gmail.com>
2022-07-26 15:47:45 +0530

    unused Delta corrected

--- WalkingEnemy.gd
+++ WalkingEnemy.gd
@@ -7,7 +7,7 @@ onready var edge_check_right = $EdgeCheckRight
 onready var edge_check_left = $EdgeCheckLeft
 onready var Sprite = $AnimatedSprite
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var found_wall = is_on_wall()
 	var found_edge = not edge_check_left.is_colliding() or not edge_check_right.is_colliding()
 	if found_wall or found_edge:
