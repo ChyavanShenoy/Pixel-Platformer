8ad49531b6cf564af83a7e8daa45c8c1c90c0d94
Chyavan Shenoy <32121448+ChyavanShenoy@users.noreply.github.com>
2022-07-30 05:12:50 +0530

    Ladder and Climb
    Added State machine and Ladder system to the world

--- Player.gd
+++ Player.gd
@@ -6,6 +6,7 @@ enum { MOVE, CLIMB }
 export(Resource) var moveData
 
 var velocity = Vector2.ZERO
+var state = MOVE
 
 onready var Sprite = $Sprite
 onready var LadderCheck = $LadderCheck
@@ -16,9 +17,38 @@ func _ready() -> void:
 	pass
 
 func _physics_process(_delta: float) -> void:
-	apply_gravity()
 	var input = Vector2.ZERO
-	input.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	input.x = Input.get_axis("left", "right")
+	input.y = Input.get_axis("ui_up", "ui_down")
+	match state:
+		MOVE: move_state(input)
+		CLIMB: climb_state(input)
+
+
+func apply_gravity():
+	velocity.y += moveData.GRAVITY
+	moveData.GRAVITY = clamp(moveData.GRAVITY, -moveData.MAX_SPEED, moveData.MAX_SPEED)
+
+func apply_friction():
+	velocity.x = move_toward(velocity.x, 0, moveData.FRICTION)
+
+func apply_acceleration(input_x_amount):
+	velocity.x = move_toward(velocity.x, input_x_amount * moveData.MAX_SPEED, moveData.ACCELERATION)
+
+func powerup(powerup_type):
+	if powerup_type == "Movement":
+		moveData = load("res://FastPlayerMovementData.tres")
+
+func is_on_ladder():
+	if not LadderCheck.is_colliding(): return false
+	var collider = LadderCheck.get_collider()
+	if not collider is Ladder: return false
+	return true
+
+func move_state(input):
+	if is_on_ladder() and Input.is_action_pressed("ui_up"): state = CLIMB
+
+	apply_gravity()
 	if input.x == 0:
 		apply_friction()
 		Sprite.set_animation("idle")
@@ -51,22 +81,11 @@ func _physics_process(_delta: float) -> void:
 		Sprite.set_animation("idle")
 		velocity.y = 0
 
-func apply_gravity():
-	velocity.y += moveData.GRAVITY
-	moveData.GRAVITY = clamp(moveData.GRAVITY, -moveData.MAX_SPEED, moveData.MAX_SPEED)
-
-func apply_friction():
-	velocity.x = move_toward(velocity.x, 0, moveData.FRICTION)
-
-func apply_acceleration(input_x_amount):
-	velocity.x = move_toward(velocity.x, input_x_amount * moveData.MAX_SPEED, moveData.ACCELERATION)
-
-func powerup(powerup_type):
-	if powerup_type == "Movement":
-		moveData = load("res://FastPlayerMovementData.tres")
-
-func is_on_ladder():
-	if not LadderCheck.is_colliding(): return false
-	var collider = LadderCheck.get_collider()
-	if not collider is Ladder: return false
-	return true
+func climb_state(input):
+	if not is_on_ladder(): state = MOVE
+	if input.length() != 0:
+		Sprite.set_animation("run")
+	else:
+		Sprite.set_animation("idle")
+	velocity = input * 50
+	velocity = move_and_slide(velocity, Vector2.UP)
